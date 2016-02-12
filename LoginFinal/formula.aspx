086953ca2f90a490dbfd8a131842e7608b2f2a72
Earth <punyawee.31bigza@gmail.com>
2016-02-12 08:59:20 +0700

    eei _p

---
+++ LoginFinal/formula.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="formula.aspx.cs" Inherits="LoginPage.formula" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>formularsuki</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container">
+        <h2>
+            ForMularSuki</h2>
+        <p>
+            สูตรการทำSuki :<asp:SqlDataSource 
+                ID="SqlDataSource1" runat="server" 
+                ConnectionString="<%$ ConnectionStrings:project-testConnectionString %>" 
+                SelectCommand="SELECT * FROM [formular]"></asp:SqlDataSource>
+            &nbsp;</p>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </div>
+    <p>
+        &nbsp;</p>
+    <div class="container">
+        <div class="rom">
+            <div class="col-md-12 col-lg-offset-5">
+                <asp:Button ID="Button1" runat="server" Text="Add" OnClick="Add_Click" />
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ LoginFinal/formula.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace LoginPage
+{
+    public partial class formula : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["project-testConnectionString"].ConnectionString);
+            conn.Open();
+            string checkname = "select * from formular ";
+            SqlCommand com = new SqlCommand(checkname, conn);
+            object obj = com.ExecuteScalar();
+            if (obj != null)
+            {
+                string message = "Name of Material is already exist !";
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append("<script type = 'text/javascript'>");
+                sb.Append("window.onload=function(){");
+                sb.Append("alert('");
+                sb.Append(message);
+                sb.Append("')};");
+                sb.Append("</script>");
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", sb.ToString());
+            }
+            else
+            {
+                try
+                {
+                    string message = "Add";
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    sb.Append("<script type = 'text/javascript'>");
+                    sb.Append("window.onload=function(){");
+                    sb.Append("alert('");
+                    sb.Append(message);
+                    sb.Append("')};");
+                    sb.Append("</script>");
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", sb.ToString());
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error : " + ex.ToString());
+                }
+                Response.Redirect("stock_manage.aspx");
+            }
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
